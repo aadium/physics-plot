57d3fd442cf742875b692439ed022c1c7209042d
Aadi Umrani <47914987+aadium@users.noreply.github.com>
2023-12-03 20:10:03 -0500

    added a graph

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import Chart from "chart.js/auto";
+import colors from "./theme/colors";
 
-function App() {
+const App = () => {
+  const chartRef = useRef(null);
+
+  let [velocity, setVelocity] = useState (0);
+
+  const handleVelocityChange = (newVel) => {
+    setVelocity(newVel);
+  };
+
+  const generateDataPoints = (equation) => {
+    const dataPoints = [];
+    for (let x = -10; x <= 10; x += 0.5) {
+      const y = equation(x);
+      dataPoints.push({ x, y });
+    }
+    return dataPoints;
+  };
+  
+  const quadraticEquation = (x) => velocity*(x*x);
+  const dataPoints = generateDataPoints(quadraticEquation);
+  
+  const data = {
+    datasets: [
+      {
+        label: 'Graph',
+        backgroundColor: colors.graphLineColor1,
+        borderColor: colors.graphLineColor1,
+        data: dataPoints,
+      },
+    ],
+  };
+
+  useEffect(() => {
+    document.body.style.backgroundColor = colors.backgroundColor;
+    const canvas = chartRef.current;
+    if (canvas.chart) {
+      canvas.chart.destroy();
+    }
+    const ctx = canvas.getContext("2d");
+    canvas.chart = new Chart(ctx, {
+      type: 'line',
+      data: data,
+      options: {
+        scales: {
+          x: {
+            type: 'linear',
+            position: 'bottom',
+            gridLines: {
+              display: true,
+            },
+            grid : {
+              color: function(context) {
+                if (context.tick.value == 0) {
+                  return colors.axisColor;
+                } else {
+                  return colors.gridlineColor;
+                }
+              },
+            }
+          },
+          y: {
+            type: 'linear',
+            position: 'left',
+            gridLines: {
+              display: true, // Set to true to display grid lines for the x-axis
+            },
+            grid : {
+              color: function(context) {
+                if (context.tick.value == 0) {
+                  return colors.axisColor;
+                } else {
+                  return colors.gridlineColor;
+                }
+              },
+            }
+          },
+        },
+      },
+    });
+    return () => {
+      if (canvas.chart) {
+        canvas.chart.destroy();
+      }
+    };
+  }, [data]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <div style={{ maxWidth: '600px', maxHeight: '700px' }}>
+        <canvas ref={chartRef} style={{ width: '100%', height: '100%' }} />
+      </div>
+    </center>
   );
-}
+};
 
 export default App;
---
+++ src/theme/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+    backgroundColor : '#171717',
+    primaryColor : '#3498db',
+    graphLineColor1 : '#3498db',
+    graphLineColor2 : '#2ecc71',
+    graphLineColor3 : '#f39c12',
+    gridlineColor: '#ffffff10',
+    axisColor : '#FFFFFF'
+}
+
+module.exports = colors
