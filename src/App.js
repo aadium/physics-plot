2777f78f42f0ef622711d8c103296ea482a944e3
Aadi Umrani <47914987+aadium@users.noreply.github.com>
2023-12-03 23:10:50 -0500

    Added parabolic trajectory graph

--- src/App.js
+++ src/App.js
@@ -2,27 +2,52 @@ import React, { useEffect, useRef, useState } from "react";
 import Chart from "chart.js/auto";
 import colors from "./theme/colors";
 
-const App = () => {
+const g = 9.81;
+
+function App() {
   const chartRef = useRef(null);
+  var tOfFlight = 0;
 
-  let [velocity, setVelocity] = useState (0);
+  const [velocity, setVelocity] = useState(0);
+  const [angle, setAngle] = useState(0);
+  const [vInputValue, setVInputValue] = useState("");
+  const [aInputValue, setAInputValue] = useState("");
 
-  const handleVelocityChange = (newVel) => {
-    setVelocity(newVel);
+  const handleVelocityChange = () => {
+    const newVel = parseFloat(vInputValue);
+    if (!isNaN(newVel)) {
+      setVelocity(newVel);
+    }
   };
 
-  const generateDataPoints = (equation) => {
-    const dataPoints = [];
-    for (let x = -10; x <= 10; x += 0.5) {
-      const y = equation(x);
-      dataPoints.push({ x, y });
+  const handleAngleChange = () => {
+    const newAng = parseFloat(aInputValue);
+    if (!isNaN(newAng)) {
+      setAngle(newAng);
     }
+  };
+
+  const generateDataPoints = () => {
+    const dataPoints = [];
+    tOfFlight = (2 * velocity * Math.sin(angle * (Math.PI / 180))) / g;
+  
+    if (tOfFlight > 0) {
+      console.log("Total Time:", tOfFlight);
+  
+      for (let t = 0; t <= tOfFlight + 0.00001; t += 0.005) {
+        const x = velocity * Math.cos(angle * (Math.PI / 180)) * t;
+        const y = velocity * Math.sin(angle * (Math.PI / 180)) * t - (0.5 * g * t ** 2);
+        dataPoints.push({ x: +x.toFixed(2), y: +y.toFixed(2) });
+      }
+    } else {
+      console.error("Invalid input parameters. Please provide valid values.");
+    }
+  
     return dataPoints;
   };
-  
-  const quadraticEquation = (x) => velocity*(x*x);
-  const dataPoints = generateDataPoints(quadraticEquation);
-  
+
+  const dataPoints = generateDataPoints();
+
   const data = {
     datasets: [
       {
@@ -30,6 +55,8 @@ const App = () => {
         backgroundColor: colors.graphLineColor1,
         borderColor: colors.graphLineColor1,
         data: dataPoints,
+        pointRadius: 0,
+        pointHitRadius: 0, 
       },
     ],
   };
@@ -52,31 +79,31 @@ const App = () => {
             gridLines: {
               display: true,
             },
-            grid : {
-              color: function(context) {
-                if (context.tick.value == 0) {
+            grid: {
+              color: function (context) {
+                if (context.tick.value === 0) {
                   return colors.axisColor;
                 } else {
                   return colors.gridlineColor;
                 }
               },
-            }
+            },
           },
           y: {
             type: 'linear',
             position: 'left',
             gridLines: {
-              display: true, // Set to true to display grid lines for the x-axis
+              display: true,
             },
-            grid : {
-              color: function(context) {
-                if (context.tick.value == 0) {
+            grid: {
+              color: function (context) {
+                if (context.tick.value === 0) {
                   return colors.axisColor;
                 } else {
                   return colors.gridlineColor;
                 }
               },
-            }
+            },
           },
         },
       },
@@ -86,15 +113,34 @@ const App = () => {
         canvas.chart.destroy();
       }
     };
-  }, [data]);
+  }, [angle, velocity]);
 
   return (
     <center>
       <div style={{ maxWidth: '600px', maxHeight: '700px' }}>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter velocity in m/s"
+            value={vInputValue}
+            onChange={(e) => setVInputValue(e.target.value)}
+          />
+          <button onClick={handleVelocityChange}>Update Velocity</button>
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter angle in degrees"
+            value={aInputValue}
+            onChange={(e) => setAInputValue(e.target.value)}
+          />
+          <button onClick={handleAngleChange}>Update Angle</button>
+        </div>
         <canvas ref={chartRef} style={{ width: '100%', height: '100%' }} />
+        <input value={tOfFlight}/>
       </div>
     </center>
   );
-};
+}
 
 export default App;
