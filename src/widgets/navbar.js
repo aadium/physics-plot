9f4c58a13e90192d41f463e5f3eba6582263666a
Aadi Umrani <47914987+aadium@users.noreply.github.com>
2023-12-07 22:28:30 -0500

    modified homepage and navbar UI

--- src/widgets/navbar.js
+++ src/widgets/navbar.js
@@ -10,25 +10,32 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <div className="navbar-logo">
-          <a href="/"><img src={navBarIcon} height={45}/></a>
-        </div>
+    <div>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <div className="navbar-logo">
+            <a href="/"><img src={navBarIcon} height={45}/></a>
+          </div>
 
-        <div className={`navbar-links ${isMobileMenuOpen ? 'open' : ''}`}>
-          <a href="/projectilemotion">Projectile</a>
-          <a href="/simpleharmonicmotion">Simple Harmonic</a>
-          <a href="/waveequation">Wave</a>
-        </div>
+          <div className={`navbar-links ${isMobileMenuOpen ? 'open' : ''}`}>
+            <a href="/projectilemotion">Projectile</a>
+            <a href="/simpleharmonicmotion">Simple Harmonic</a>
+            <a href="/waveequation">Wave</a>
+          </div>
 
-        <div className="navbar-toggle" onClick={toggleMobileMenu}>
-          <span>A</span>
-          <span>B</span>
-          <span>C</span>
+          <div className="navbar-toggle" onClick={toggleMobileMenu}>
+            <span>A</span>
+            <span>B</span>
+            <span>C</span>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+    </div>
   );
 };
 
